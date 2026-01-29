72b27226998f1cb3682bcdb9fbeb92649fcd5a89
madelinekriha <madelinekriha@gmail.com>
2026-01-28 20:19:57 -0500

    Update hello.java
    pt 2

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	
 	public static void main(String[] args) {
-        System.out.println("goodbye, World!"); 
+        System.out.println("goodbye, World! pt 2"); 
     }
 
 }
